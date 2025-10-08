067c1a80d0ca4e9a63599134c07136a8e7a71af5
Chenxuan <chenxuan.meng@mail.utoronto.ca>
2025-10-08 10:53:34 -0400

    revise question 3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,8 +32,8 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mv rawdata data/raw
 # 3. List the contents of the ./data/raw directory
-cd data/raw
-ls
+cd data
+ls raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 cd ..
 mkdir -p processed/server_logs processed/user_logs processed/event_logs
