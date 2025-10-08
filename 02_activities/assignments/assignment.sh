27fb3e0f05a117dc5773a9a206c40c870f9684ed
Chenxuan <chenxuan.meng@mail.utoronto.ca>
2025-10-08 10:25:21 -0400

    Fix Q3: correct ls command to run in data/raw directory

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,9 +32,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mv rawdata data/raw
 # 3. List the contents of the ./data/raw directory
-cd data
-ls 
-cd raw
+cd data/raw
 ls
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 cd ..
